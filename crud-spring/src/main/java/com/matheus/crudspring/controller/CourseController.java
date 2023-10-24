eabd554cb30555276a8105171d5ac586d690a4bd
Matheus A. Barreto <matheus.a.barreto12@gmail.com>
2023-10-24 15:01:26 -0300

    A service layer on API and exception handling

--- crud-spring/src/main/java/com/matheus/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/matheus/crudspring/controller/CourseController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.matheus.crudspring.model.Course;
 import com.matheus.crudspring.repository.CourseRepository;
+import com.matheus.crudspring.service.CourseService;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
@@ -28,49 +29,40 @@ import lombok.AllArgsConstructor;
 @RestController
 @RequestMapping("/api/courses")
 @Component
-@AllArgsConstructor
 public class CourseController {
 
 
-    private final CourseRepository courseRepository;
-    
+
+    private final CourseService courseService;
+
+    public CourseController(CourseService courseService){
+
+        this.courseService = courseService;
+    }
+
     @GetMapping
     public List<Course> list () {
-        return courseRepository.findAll();
+        return courseService.list();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Course> findById(@PathVariable @NotNull @Positive Long id) {
-        return courseRepository.findById(id)
-            .map(recordFound -> ResponseEntity.ok().body(recordFound))
-            .orElse(ResponseEntity.notFound().build());
+    public Course findById(@PathVariable @NotNull @Positive Long id) {
+        return courseService.findById(id);
     }
 
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
     public Course create(@RequestBody @Valid Course course) {
-        return courseRepository.save(course);
+        return courseService.create(course);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Course> update(@PathVariable @NotNull @Positive Long id, @RequestBody @Valid Course course) {
-        return courseRepository.findById(id)
-        .map(recordFound -> {
-            recordFound.setName(course.getName());
-            recordFound.setCategory(course.getCategory());
-            Course updated = courseRepository.save(recordFound);
-            return ResponseEntity.ok().body(updated);
-        })
-        .orElse(ResponseEntity.notFound().build());
+    public Course update(@PathVariable @NotNull @Positive Long id, @RequestBody @Valid Course course) {
+        return courseService.update(id, course);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable @NotNull @Positive Long id) {
-         return courseRepository.findById(id)
-        .map(recordFound -> {
-            courseRepository.deleteById(id);
-            return ResponseEntity.noContent().<Void>build();
-        })
-        .orElse(ResponseEntity.notFound().build());
+    public void delete(@PathVariable @NotNull @Positive Long id) {
+        courseService.delete(id);
     }
 }
--- crud-spring/src/main/java/com/matheus/crudspring/service/CourseService.java
+++ crud-spring/src/main/java/com/matheus/crudspring/service/CourseService.java
@@ -1,15 +1,15 @@
-package com.matheus.service;
+package com.matheus.crudspring.service;
 
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import com.matheus.crudspring.exception.RecordNotFoundException;
 import com.matheus.crudspring.model.Course;
 import com.matheus.crudspring.repository.CourseRepository;
 
@@ -19,41 +19,37 @@ import jakarta.validation.constraints.Positive;
 
 @Validated
 @Service
-public class CoursesService {
-    
+public class CourseService {
     private final CourseRepository courseRepository;
 
-    public CoursesService(CourseRepository courseRepository) {
+    public CourseService(CourseRepository courseRepository){
         this.courseRepository = courseRepository;
     }
 
-    public List<Course> list(){
+    public List<Course> list () {
         return courseRepository.findAll();
     }
-    
-    public Optional<Course> findById(@PathVariable @NotNull @Positive Long id) {
-        return courseRepository.findById(id);
+
+    public Course findById(@PathVariable @NotNull @Positive Long id) {
+        return courseRepository.findById(id).orElseThrow(() -> new RecordNotFoundException(id));
     }
-    
-    public Course create(@Valid Course course) {
+
+     public Course create(@Valid Course course) {
         return courseRepository.save(course);
     }
 
-    public Optional<Course> update( @NotNull @Positive Long id, @Valid Course course) {
+
+    public Course update( @NotNull @Positive Long id, @Valid Course course) {
         return courseRepository.findById(id)
         .map(recordFound -> {
             recordFound.setName(course.getName());
             recordFound.setCategory(course.getCategory());
             return courseRepository.save(recordFound);
-        });
+        }).orElseThrow(() -> new RecordNotFoundException(id));
     }
 
-    public boolean delete(@PathVariable @NotNull @Positive Long id) {
-        return courseRepository.findById(id)
-       .map(recordFound -> {
-           courseRepository.deleteById(id);
-            return true;
-       })
-       .orElse(false);
+    public void delete(@PathVariable @NotNull @Positive Long id) {
+        
+        courseRepository.delete((courseRepository.findById(id).orElseThrow(() -> new RecordNotFoundException(id))));
    }
 }
